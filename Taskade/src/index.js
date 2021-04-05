575cff70ba7a7dcecf2654dbc57cba44c199fc0c
unknown <nikkhil888@gmail.com>
2021-04-05 12:41:45 +0530

    connect to database

--- Taskade/src/index.js
+++ Taskade/src/index.js
@@ -1,9 +1,13 @@
 const { ApolloServer, gql } = require('apollo-server');
+const { MongoClient} = require('mongodb');
+const dotenv = require('dotenv');
+dotenv.config();
+const {DB_URI,DB_NAME} = process.env;
 
 const books = [
   {
     title: 'The Awakening',
-    author: 'Kate Chopin',
+    author: 'Kate Chon',
   },
   {
     title: 'City of Glass',
@@ -11,18 +15,12 @@ const books = [
   },
 ];
 
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
+
 const typeDefs = gql`
-    # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
     books: [Book]
   }
 
-  # This "Book" type defines the queryable fields for every book in our data source.
   type Book {
     title: String
     author: String
@@ -32,18 +30,32 @@ const typeDefs = gql`
 `;
 
 
-// Resolvers define the technique for fetching the types defined in the
-// schema. This resolver retrieves books from the "books" array above.
 const resolvers = {
   Query: {
-    books: () => books,
+    books: (root,data,context) => {
+      return books;
+    },
   },
 };
 
+const start = async () => {
+  const client = new MongoClient(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+  await client.connect();
+  const db = client.db(DB_NAME);
 
-const server = new ApolloServer({ typeDefs, resolvers });
+  const context = {
+    db,
+  }
 
-// The `listen` method launches a web server.
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
-});
+  const server = new ApolloServer({ 
+    typeDefs, 
+    resolvers,
+    context
+  });
+
+  server.listen().then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+  });
+}
+
+start();
